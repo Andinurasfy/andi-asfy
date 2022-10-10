e5814e74388a4b564e22b33cd6d0beb98023cb40
andi nur asfy <andinurasfi@gmail.com>
2022-10-10 13:33:03 +0700

    upload tugas1

--- app/src/main/java/com/unmus/androidtraining/Tugas/Tugasprofile.kt
+++ app/src/main/java/com/unmus/androidtraining/Tugas/Tugasprofile.kt
@@ -1,19 +1,18 @@
-package com.unmus.androidtraining
+package com.unmus.androidtraining.Tugas
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -23,31 +22,18 @@ import androidx.compose.ui.unit.dp
 import com.unmus.androidtraining.ui.theme.AndroidTrainingTheme
 import com.unmus.androidtraining.ui.theme.Purple200
 
-class MainActivity : ComponentActivity() {
+class Tugasprofile : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidTrainingTheme{
-                Conversation(GenerateDataDummyUser())
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val user = User("Unmus", "Papua", 20)
+                    MenampilkanPesan(user)
+                }
             }
         }
     }
-}
-
-fun GenerateDataDummyUser(): ArrayList<User>{
-    var users: ArrayList<User> = arrayListOf()
-
-    // buat objek user
-    var user = User("Siti", "Papua asdfasdfaas asdfsafasd asdfsadfa sadfasdfas asdfsafa", 20)
-    users.add(user)
-
-    user = User("Maxiel", "Sorongsadfasdfa  asdfsadfasf asdfsafafsa asdfsafsafas", 30)
-    users.add(user)
-
-    user = User("Alex", "Merauke asdfsafsa asdfasfa asdfasdfsa asdfsdafafsaasdf asdfadfasadfasdf sadf", 25)
-    users.add(user)
-
-    return users
 }
 
 data class User(
@@ -57,13 +43,13 @@ data class User(
 )
 
 @Composable
-fun Pesan(user: User){
+fun MenampilkanPesan(user: User){
     Row(
         modifier = Modifier.padding(all = 8.dp)
     ) {
         Image(painter =
         painterResource(id = R.drawable.musamus),
-            contentDescription = "Uni musamus",
+            contentDescription = "musamus",
             modifier = Modifier
                 // Set image size to 40 dp
                 .size(40.dp)
@@ -72,23 +58,13 @@ fun Pesan(user: User){
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape),
             contentScale = ContentScale.Crop)
         Spacer(modifier = Modifier.width(8.dp))
-
-        // We keep track if the message is expanded or not in this
-        // variable
-        var isExpanded by remember { mutableStateOf(false) }
-
-
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+        Column {
             Text(text = user.name, color = Purple200)
             Spacer(modifier = Modifier.height(8.dp))
             Surface(shape = MaterialTheme.shapes.medium, elevation = 2.dp) {
                 Text(
                     text = user.address,
                     modifier = Modifier.padding(all = 4.dp),
-                    // If the message is expanded, we display all its content
-                    // otherwise we only display the first line
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-
                     style = MaterialTheme.typography.body2
                 )
             }
@@ -99,15 +75,16 @@ fun Pesan(user: User){
 @Preview
 @Composable
 fun PreviewMenampilkanPesan(){
-    val user = User("Unmus", "Papua", 30)
-    Pesan(user)
+    val user = User("Unmus", "Papua", 20)
+    MenampilkanPesan(user)
 }
 
+
 @Composable
-fun Conversation(users: ArrayList<User>){
-    LazyColumn {
+fun Conversation(users: List<User>) {
+    LazyRow {
         items(users) { user ->
-            Pesan(user = user)
+            MenampilkanPesan(user = user)
         }
     }
 }
